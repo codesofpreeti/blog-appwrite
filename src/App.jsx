ed40566fe42aad12d643b1bcc68d9aa727b47556
codesofpreeti <preetiipriyaa@gmail.com>
2024-06-27 19:27:57 +0530

    -first

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import conf from './conf/conf';
+
+import './App.css'
+
+function App() {
+  console.log(conf.appwriteUrl);
+  return (
+    <>
+      <h1>Blog application</h1>
+    </>
+  )
+}
+
+export default App
