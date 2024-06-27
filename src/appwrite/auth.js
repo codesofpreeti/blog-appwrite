47efe27088080c5392a682383bb2643b8bb5d19a
codesofpreeti <preetiipriyaa@gmail.com>
2024-06-27 20:54:31 +0530

    more auth

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,5 +1,6 @@
 import conf from "../conf/conf";
 import { Client, Account, ID } from "appwrite";
+
 export class AuthService {
   client = new Client();
   account;
@@ -11,39 +12,42 @@ export class AuthService {
   }
   async createAccount({ email, password, name }) {
     try {
-      const userAcc=await this.account.create(ID.unique(), email, password, name);
-      if(userAcc){
+      const userAcc = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAcc) {
         //next method
-        return this.login({email,password});
-      }
-      else{
+        return this.login({ email, password });
+      } else {
         return userAcc;
       }
     } catch (error) {
       throw error;
     }
   }
-  async login({email,password}){
+  async login({ email, password }) {
     try {
-        return await this.account.createEmailPasswordSession(email,password);
+      return await this.account.createEmailPasswordSession(email, password);
     } catch (error) {
-        throw error;
+      throw error;
     }
   }
-  async getCurrentUser(){
+  async getCurrentUser() {
     try {
-        return await this.account.get();
+      return await this.account.get();
     } catch (error) {
-        throw error;
+      throw error;
     }
     return null;
-
   }
-  async logout(){
+  async logout() {
     try {
-        return await this.account.deleteSessions();
+      return await this.account.deleteSessions();
     } catch (error) {
-        console.log("no user logged in")
+      console.log("no user logged in");
     }
   }
 }
--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -1,2 +1,108 @@
 import conf from "../conf/conf";
-import { Client, Account, ID } from "appwrite";
+import { Client, Databases, Storage, Query, ID } from "appwrite";
+
+export class Service {
+  client = new Client();
+  databases;
+  bucket;
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.databases = new Databases(this.client);
+    this.bucket = new Storage(this.client);
+  }
+  async createPost({ title, slug, featuredImg, content, status, userId }) {
+    try {
+      return await this.databases.createDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+        { title, slug, content, featuredImg, userId, status }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async updatePost(slug, { title, featuredImg, content, status }) {
+    try {
+      return await this.databases.updateDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImg,
+          status,
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async deletePost(slug) {
+    try {
+      await this.databases.deleteDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug
+      );
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+  async getPost(slug) {
+    try {
+      return await this.databases.listDocuments(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async getPosts(queries = [Query.equal("status", "active")]) {
+    try {
+      return await this.databases.listDocuments(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        queries
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  //file upload
+  async uploadFile(file) {
+    try {
+      await this.bucket.createFile(conf.appwriteBucketId, ID.unique(), file);
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+  //file delete
+  async deleteFile(fileId) {
+    try {
+      await this.bucket.deleteFile(conf.appwriteBucketId, fileId);
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+  async getFilePreview(fileId){
+    try {
+        await this.bucket.getFilePreview(conf.appwriteBucketId,fileId);
+    } catch (error) {
+        console.log(error)
+    }
+  }
+}
+const service = new Service();
+export default service;
