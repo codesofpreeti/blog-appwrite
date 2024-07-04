f1fa884700758a8215e6209c33965c6e6b4b9b4c
codesofpreeti <preetiipriyaa@gmail.com>
2024-07-04 22:30:46 +0530

    forwardRef stuff

---
+++ src/components/Input.jsx
@@ -0,0 +1,20 @@
+import React, { useId } from "react";
+
+const Input = React.forwardRef(function Input(
+  { label, type = "text", className = "", ...props },
+  ref
+) {
+    const id= useId();
+  return (
+    <div className="w-full ">
+      {label && (
+        <label className="block mb-1 pl-1" htmlFor={props.id}>
+          {label}
+        </label>
+      )}
+      <input type={type} className={`${className} rounded-lg border w-full`} ref={ref} {...props} id={id}/>
+    </div>
+  );
+});
+
+export default Input;
